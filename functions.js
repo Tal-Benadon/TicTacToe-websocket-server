1d37aed8d604d2cafd7fd98425296daa0f3b413b
Tal Ben Adon <talbenadon2@gmail.com>
2024-05-14 23:48:12 +0300

    tal

--- functions.js
+++ functions.js
@@ -8,6 +8,7 @@ const createBoard = () => {
                 symbol: '',
                 animationTrigger: 0,
                 isInactive: false,
+                isPlayed: false,
                 location: [i, j]
             })
         }
@@ -16,4 +17,99 @@ const createBoard = () => {
     return gameBoard
 }
 
-module.exports = { createBoard }
+
+const updateSymbol = (row, col, symbol, gameBoard) => {
+    console.log(gameBoard[row][col]);
+    if (!gameBoard[row][col].isPlayed) {
+        console.log("hihihihihi");
+        gameBoard[row][col].symbol = symbol
+        gameBoard[row][col].isPlayed = true
+        return [...gameBoard]
+    } else {
+        console.log("false..");
+        return false
+    }
+}
+
+const checkBoard = (gameBoard, row, col, symbol) => {
+    let iterations = 3
+    let column = []
+    let cross1 = []
+    let cross2 = []
+    let gameRow = gameBoard[row]
+    let j = 0
+    let k = 0
+
+    for (let i = 0; i < iterations; i++) {
+        cross1.push(gameBoard[j][i])
+        j++
+    }
+    for (let i = iterations - 1; i >= 0; i--) {
+        cross2.push(gameBoard[k][i])
+        k++
+    }
+    for (let i = 0; i < iterations; i++) {
+        column.push(gameBoard[i][col]);
+    }
+    if (gameRow.every(cell => cell.symbol === gameRow[0].symbol && cell.symbol !== '')) {
+
+        console.log({ gameRow });
+        return {
+
+            gameWinner: symbol, gameEnded: true, gameBoard: gameBoard.map(row =>
+                row.map(cell =>
+                    !includesSubArray(gameRow.map(value => value.location), cell.location) ?
+                        { ...cell, isInactive: true }
+                        : cell
+                )
+            )
+        }
+    }
+    if (column.every(cell => cell.symbol === column[0].symbol && cell.symbol !== '')) {
+        console.log({ column });
+        return {
+            gameWinner: symbol, gameEnded: true, gameBoard: gameBoard.map(row =>
+                row.map(cell =>
+                    !includesSubArray(column.map(value => value.location), cell.location) ?
+                        { ...cell, isInactive: true }
+                        : cell
+                )
+            )
+        }
+    }
+    if (cross1.every(cell => cell.symbol === cross1[0].symbol && cell.symbol !== '')) {
+        return {
+            gameWinner: symbol, gameEnded: true, gameBoard: gameBoard.map(row =>
+                row.map(cell =>
+                    !includesSubArray(cross1.map(value => value.location), cell.location) ?
+                        { ...cell, isInactive: true }
+                        : cell
+                )
+            )
+        }
+    }
+    if (cross2.every(cell => cell.symbol === cross2[0].symbol && cell.symbol !== '')) {
+        return {
+            gameWinner: symbol, gameEnded: true, gameBoard: gameBoard.map(row =>
+                row.map(cell =>
+                    !includesSubArray(cross2.map(value => value.location), cell.location) ?
+                        { ...cell, isInactive: true }
+                        : cell
+                )
+            )
+        }
+
+    } else {
+        return false
+    }
+}
+
+const includesSubArray = (mainArray, subArray) => {
+    return mainArray.some(element =>
+        Array.isArray(element) &&
+        element.length === subArray.length &&
+        element.every((value, index) => value === subArray[index])
+    );
+}
+
+module.exports = { createBoard, checkBoard, updateSymbol }
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors())
 
 const server = createServer(app)
 const io = new Server(server, { cors: { origin: "*", methods: "*" } })
-const { createBoard } = require('./functions')
+const { createBoard, updateSymbol, checkBoard } = require('./functions')
 app.get('/test', (req, res) => res.send("Yessss"))
 
 
@@ -48,7 +48,7 @@ io.on('connection', (socket) => {
 
         rooms[roomId] = { users: [{ userId: data.userId }], capacity: 2 }
         updateUsersCount(rooms[roomId])
-        console.log(rooms);
+        // console.log(rooms);
         socket.emit("create-game", { roomId })
         io.in(roomId).emit("game-code", { gameCode: roomId })
         io.in(roomId).emit("gamejoin-alert", `${data.userId} joined room number ${roomId}`)
@@ -65,7 +65,7 @@ io.on('connection', (socket) => {
 
                 socket.roomCode = roomId;
 
-                console.log(rooms[roomId].users);
+                // console.log(rooms[roomId].users);
                 rooms[roomId]?.users?.push({ userId: data.userId })
                 updateUsersCount(rooms[data.gameCode])
                 let room = io.sockets.adapter.rooms.get(roomId)
@@ -82,10 +82,10 @@ io.on('connection', (socket) => {
 
 
     socket.on("symbol-choice", (data) => {
-        console.log("my choice: ", data)
+        // console.log("my choice: ", data)
         let roomId = socket.roomCode;
 
-        console.log(roomId)
+        // console.log(roomId)
         const myUserId = rooms[roomId].users.find(user => socket.id === user.userId);
 
 
@@ -97,25 +97,68 @@ io.on('connection', (socket) => {
 
         opponent.symbol = setSymbol(data)
 
-        console.log(opponent)
-        console.log(myUserId)
+        // console.log(opponent)
+        // console.log(myUserId)
 
         socket.on("sides-chosen", (data) => {
             if (data.complete) {
                 let roomId = socket.roomCode;
                 let complete = data.complete
-                console.log(data.chosenSymbol);
+                // console.log(data.chosenSymbol);
                 let chosenSymbol = data.chosenSymbol
 
                 let opponentSymbol = setSymbol(chosenSymbol)
 
                 rooms[roomId].gameBoard = createBoard()
+                rooms[roomId].movesCounter = 0
+                console.log(rooms[roomId].gameBoard);
                 socket.to(roomId).emit("sides-chosen", { complete, opponentSymbol })
                 let newGameBoard = rooms[roomId].gameBoard
-                io.in(roomId).emit("create-board", { gameBoard: newGameBoard })
+                rooms[roomId].currentTurn = rooms[roomId].users.find(user => user.symbol === 'X').userId
+                // console.log(rooms[roomId]);
+                let initialTurn = rooms[roomId].currentTurn
+                io.in(roomId).emit("create-board", { gameBoard: newGameBoard, initialTurn })
             }
         })
 
+
+    })
+
+    socket.on("game-move", (data) => {
+        let roomId = socket.roomCode;
+        let roomBoard = rooms[roomId].gameBoard
+        let location = data.location
+        let symbol = data.mySymbol
+        rooms[roomId].gameEnded = false
+        let result = updateSymbol(location[0], location[1], symbol, roomBoard)
+        console.log(result);
+        if (result) {
+            rooms[roomId].movesCounter++
+            rooms[roomId].gameBoard = result
+            let gameBoard = rooms[roomId].gameBoard
+            let checkUp = checkBoard(gameBoard, location[0], location[1], symbol)
+            rooms[roomId].currentTurn = rooms[roomId].users.find(user => user.userId !== socket.id).userId
+            io.in(roomId).emit("game-move", { gameBoard: rooms[roomId].gameBoard, newTurn: rooms[roomId].currentTurn })
+            // let gameEnded = false
+            let gameWinner = null
+            if (checkUp) {
+                rooms[roomId].gameBoard = checkUp.gameBoard
+                rooms[roomId].gameEnded = checkUp.gameEnded
+                let gameEnded = rooms[roomId].gameEnded
+                gameWinner = checkUp.gameWinner
+                io.in(roomId).emit("game-end", { gameEnded: gameEnded, gameWinner: gameWinner, gameBoard: rooms[roomId].gameBoard })
+            }
+            if (!checkUp && rooms[roomId].movesCounter === 9) {
+                rooms[roomId].gameEnded = true
+                let gameEnded = rooms[roomId].gameEnded
+                gameWinner = 'Draw'
+                io.in(roomId).emit("game-end", { gameEnded: gameEnded, gameWinner })
+            }
+        } else {
+            socket.emit("illegal-move", { illegal: true, alert: "Illegal move" })
+        }
+        // console.log(rooms[roomId]);
+        // console.log(rooms[roomId].gameBoard);
     })
 
 })
